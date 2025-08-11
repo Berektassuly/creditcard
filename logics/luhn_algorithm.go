a1ed123a3ba759c4b7353649885fee802cefcc31
Berektasuly <berektasulymuhammedali@gmail.com>
2025-08-11 13:44:31 +0600

    repair luhn_algorithm and fixed main.go, import packages and validation

--- logics/luhn_algorithm.go
+++ logics/luhn_algorithm.go
@@ -5,27 +5,29 @@ import (
 	"strings"
 )
 
-fucn IsValid(cardNumber string) bool {
-	number = strings.TrimSpace(cardNumber)
-	if len(number) < 2 {
+func IsValid(cardNumber string) bool {
+	cardNumber = strings.TrimSpace(cardNumber)
+	if len(cardNumber) < 2 {
 		return false
 	}
 
 	var sum int
-	parity := range(number) % 2
+	parity := len(cardNumber) % 2
 
-	for i, r := range number {
+	for i, r := range cardNumber {
 		digit, err := strconv.Atoi(string(r))
 		if err != nil {
 			return false
 		}
-			if i % 2 == parity {
-				digit *= 2
-				if degit > 9 {
+		
+		if i%2 == parity {
+			digit *= 2
+			if digit > 9 {
 				digit -= 9
 			}
+		}
+
 		sum += digit
 	}
-	return sum % 10 == 0
-	}
+	return sum%10 == 0
 }
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"creditcard/logics"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Ошибка: Необходимо передать номер карты в качестве аргумента.")
+		os.Exit(1)
+	}
+	cardNumber := os.Args[1]
+	if logics.IsValid(cardNumber) {
+		// По заданию нужно выводить "OK"
+		fmt.Println("OK")
+		os.Exit(0)
+	} else {
+		// По заданию нужно выводить "INCORRECT" в stderr
+		fmt.Fprintln(os.Stderr, "INCORRECT")
+		os.Exit(1)
+	}
+}
