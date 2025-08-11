d0ce8a90f59783315b194e46c9f3321de6aecc22
Berektasuly <berektasulymuhammedali@gmail.com>
2025-08-12 01:22:32 +0600

    fixed case information

--- main.go
+++ main.go
@@ -36,22 +36,22 @@ func main() {
 		if len(os.Args) < 3 {
 			fmt.Fprintln(os.Stderr, "Ошибка: Необходимо передать номер карты для получения информации.")
 			os.Exit(1)
+		}
 			cardnumber := os.Args[2]
-
 			isValid := logics.IsValid(cardnumber)
 			brand := logics.FindBrand(cardnumber)
 			issuer := logics.FindIssuer(cardnumber)
 
-			if isValid {
+		if isValid {
 				fmt.Printf("Card Number: %s\n", cardnumber)
 				fmt.Printf("Brand: %s\n", brand)
 				fmt.Printf("Issuer: %s\n", issuer)
-			} else {
+		} else {
 				fmt.Fprintln(os.Stderr, "Card number is invalid.")
 				os.Exit(1)
-			}
 		}
-		default:
+		
+	default:
 		fmt.Fprintf(os.Stderr, "Ошибка: Неизвестная команда '%s'.\n", command)
 		os.Exit(1)
 	}
