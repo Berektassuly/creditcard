f0f22b663718af26e81ea233b921c1783580b4e1
Berektasuly <berektasulymuhammedali@gmail.com>
2025-08-12 14:35:45 +0600

    fixed small bugs in readfiledata. created new function: findmatch - finds the most accurate match || all changes in data.go

--- logics/data.go
+++ logics/data.go
@@ -1,15 +1,15 @@
 package logics
 
 import (
-	"os"
-	"fmt"
 	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func ReadDataFile (filepath string) (map[string]string, error) {
 	file, err := os.Open(filepath)
-	if err !-= nil {
+	if err != nil {
 		return nil, fmt.Errorf("ошибка открытия файла: %s: %v", filepath, err)
 	}
 	defer file.Close()
@@ -18,12 +18,12 @@ func ReadDataFile (filepath string) (map[string]string, error) {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
-			continiue
+			continue
 		}
 		parts := strings.SplitN(line, ":", 2)
 		if len(parts) == 2 {
 			name := strings.TrimSpace(parts[0])
-			prefix := string.TrimSpace(parts[1])
+			prefix := strings.TrimSpace(parts[1])
 			if name != "" && prefix != "" {
 				data[prefix] = name
 			}
@@ -33,4 +33,18 @@ func ReadDataFile (filepath string) (map[string]string, error) {
 		return nil, fmt.Errorf("ошибка чтения файла: %s: %v", filepath, err)
 	}
 	return data, nil
+}
+
+func FindMatch(cardNumber string, data map[string]string) string {
+	bestMatchPrefix := ""
+	foundName := "-"
+	for prefix, name := range data {
+		if strings.HasPrefix(cardNumber, prefix) {
+			if len(prefix) > len(bestMatchPrefix) {
+				bestMatchPrefix = prefix
+				foundName = name
+			}
+		}
+	}
+	return foundName
 }
