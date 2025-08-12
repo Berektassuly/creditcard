57ed0e0162baa02d3d452aaac93d19ecab769ed3
Berektasuly <berektasulymuhammedali@gmail.com>
2025-08-12 12:54:05 +0600

    updated luhn_algorithm, added lenght checker

--- logics/luhn_algorithm.go
+++ logics/luhn_algorithm.go
@@ -5,21 +5,21 @@ import (
 	"strings"
 )
 
+
 func IsValid(cardNumber string) bool {
 	cardNumber = strings.TrimSpace(cardNumber)
-	if len(cardNumber) < 2 {
+	if len(cardNumber) < 13 {
 		return false
 	}
 
 	var sum int
 	parity := len(cardNumber) % 2
-
 	for i, r := range cardNumber {
 		digit, err := strconv.Atoi(string(r))
 		if err != nil {
 			return false
 		}
-		
+
 		if i%2 == parity {
 			digit *= 2
 			if digit > 9 {
@@ -29,5 +29,35 @@ func IsValid(cardNumber string) bool {
 
 		sum += digit
 	}
+
 	return sum%10 == 0
 }
+
+func CalculateLuhnDigit
+
+// func IsValid(cardNumber string) bool {
+// 	cardNumber = strings.TrimSpace(cardNumber)
+// 	if len(cardNumber) < 2 {
+// 		return false
+// 	}
+
+// 	var sum int
+// 	parity := len(cardNumber) % 2
+
+// 	for i, r := range cardNumber {
+// 		digit, err := strconv.Atoi(string(r))
+// 		if err != nil {
+// 			return false
+// 		}
+		
+// 		if i%2 == parity {
+// 			digit *= 2
+// 			if digit > 9 {
+// 				digit -= 9
+// 			}
+// 		}
+
+// 		sum += digit
+// 	}
+// 	return sum%10 == 0
+// }
