a195aa32665a66e352401b3a0db2ac826d90e936
Berektasuly <berektasulymuhammedali@gmail.com>
2025-08-13 15:42:48 +0600

    issue is upgraded to more flexibility with data type

--- logics/issue.go
+++ logics/issue.go
@@ -9,6 +9,7 @@ import (
 )
 
 func Issue(brandData []BrandOrIssuer, issuerData []BrandOrIssuer, targetBrand, targetIssuer string) (string, error) {
+
 	var issuerPrefix string
 	for _, item := range issuerData {
 		if item.Name == targetIssuer {
@@ -16,18 +17,11 @@ func Issue(brandData []BrandOrIssuer, issuerData []BrandOrIssuer, targetBrand, t
 			break
 		}
 	}
+
 	if issuerPrefix == "" {
 		return "", fmt.Errorf("эмитент '%s' не найден", targetIssuer)
 	}
-	// var validBrandPrefixes []string
-	// for prefix, name := range brandData {
-	// 	if name == targetBrand {
-	// 		validBrandPrefixes = append(validBrandPrefixes, prefix)
-	// 	}
-	// }
-	// if len(validBrandPrefixes) == 0 {
-	// 	return "", fmt.Errorf("бренд '%s' не найден", targetBrand)
-	// }
+
 	isBrandValidForIssuer := false
 	for _, item := range brandData {
 		if item.Name == targetBrand {
@@ -37,23 +31,28 @@ func Issue(brandData []BrandOrIssuer, issuerData []BrandOrIssuer, targetBrand, t
 			}
 		}
 	}
+
 	if !isBrandValidForIssuer {
 		return "", fmt.Errorf("бренд '%s' не подходит для эмитента '%s'", targetBrand, targetIssuer)
 	}
-	lenght := 16
-	if targetBrand == "Amex" {
-		lenght = 15
+
+
+	length := 16
+	if targetBrand == "AMEX" {
+		length = 15
 	}
-	randomDigitsCount := lenght - len(issuerPrefix) - 1
+	randomDigitsCount := length - len(issuerPrefix) - 1
 
 	if randomDigitsCount < 0 {
-		return "", fmt.Errorf("эмитент '%s' слишком длинный для генерации номера", targetIssuer)
+		return "", fmt.Errorf("префикс эмитента '%s' слишком длинный для генерации номера", issuerPrefix)
 	}
+	
+	rand.Seed(time.Now().UnixNano())
 	var builder strings.Builder
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	builder.WriteString(issuerPrefix)
 	for i := 0; i < randomDigitsCount; i++ {
-		builder.WriteString(strconv.Itoa(r.Intn(10)))
+		builder.WriteString(strconv.Itoa(rand.Intn(10)))
 	}
 	baseNumber := builder.String()
 	checkDigit := CalculateLuhnDigit(baseNumber)
